6e0a2eacd7e0c58656cf2c54efcbfad9216bf223
oli59 <olivier.materne@gmail.com>
2016-06-20 16:02:44 +0200

    css/layout

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,59 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: `<h1>{{title}}</h1>
+    template: `<div class="app_title">
+    {{title}}
+    </div>
+
+    <div class="app-body">
+
+    <div class="nav">
+    Top<br>
+    Work<br>
+    epay<br>
+    </div>
+
+    <div class="canvas">
+
+
     <h2>{{activity.name}} details!</h2>
     <ul class="activities">
-        <li *ngFor="let activity of activities">
+        <li *ngFor="let activity of activities" draggable="true">
             <span class="badge">{{activity.status}}</span> {{activity.name}}
-            
         </li>
     </ul>
     <div>
         <label>name: </label> 
         <input [(ngModel)]="activity.name" placeholder="name"> 
     </div>
-    <div><label>status: </label> {{activity.status}}</div>`,
+    <div><label>status: </label> {{activity.status}}</div>
+
+    </div>
+    </div>
+
+    <div class="app_footer">
+        OM  2016
+    </div>
+    `,
+
     styles:[`
+        .canvas {
+
+            border: 1px solid black;
+            width: 100%;
+            height: 100%;
+            margin: .5em;
+        }
+        .nav{
+            float: left;
+            line-height:30px;
+            border: 1px solid black;
+            background-color:#eeeeee;
+            width:100px;
+            height: 100%;
+        }
         .activities {
+
             margin: 0 0 2em 0;
             list-style-type: none;
             padding: 0;
@@ -50,7 +88,7 @@ import { Component } from '@angular/core';
             position: relative;
             left: -1px;
             top: -4px;
-            height: 4.2em;
+            height: 4.7em;
             margin-right: .8em;
             border-radius: 4px 0 0 4px;
         }
