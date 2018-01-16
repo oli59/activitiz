d69abc98c8f96ac5ebbeccb711bb6ba5226cdb23
materne.o <lkjfs@qslfkj>
2018-01-16 16:52:34 +0100

    scheduling

--- server/business/JournallogService.go
+++ server/business/JournallogService.go
@@ -6,6 +6,7 @@ import (
   "time"
   "database/sql"
   "fmt"
+  "math/rand"
 )
 
 func CreateJournallog (a domain.Journallog) domain.Journallog {
@@ -33,10 +34,14 @@ func Schedule(maxActivities int, date time.Time) domain.Journallogs {
 
   //TODO tant que il reste du temps ou que le maxActivites n'est pas dépassé
 
-  //Random par type Auto ou DL
+  //TODO Random par type Auto ou DL
 
-  scheduledJl = append (scheduledJl, ScheduleAutomatic())
-  fmt.Println(scheduledJl);
+  //Automatic Scheduling
+  schedulableActivities := dao.GetAllSchedulableLeafs();
+  //TODO supprimer de la liste toutes les activities qui sont déjà dans un journallog du jour
+  jl := ScheduleAutomatic(schedulableActivities);
+  scheduledJl = append (scheduledJl, jl);
+  //todo retirer jl de la liste des activité schedulables
 
   dao.CreateJournallog(scheduledJl[0]);
 
@@ -44,13 +49,12 @@ func Schedule(maxActivities int, date time.Time) domain.Journallogs {
 }
 
 /*Schedule one automatic activity*/
-func ScheduleAutomatic() domain.Journallog {
+func ScheduleAutomatic(schedulableActivities domain.Activities) domain.Journallog {
   var sumCurrentPoint int64= 0;
   var sumTypicalPoint int64= 0;
-  schedulableActivities := dao.GetAllSchedulableLeafs();
 
   for _, activity := range schedulableActivities {
-    if activity.CurrentPoints.Valid {
+    if activity.CurrentPoints.Valid && activity.CurrentPoints.Int64 > 0 {
       sumCurrentPoint += activity.CurrentPoints.Int64;
     }
     if activity.TypicalDuration.Valid {
@@ -59,15 +63,36 @@ func ScheduleAutomatic() domain.Journallog {
   }
 
   //If total current points < 1/2 total typical points => 1/2 typical points to all activities
-  if sumCurrentPoint < sumCurrentPoint / 2 {
+  if sumCurrentPoint < sumTypicalPoint / 2 {
+    sumCurrentPoint = 0;
     for _, activity := range schedulableActivities {
-      activity.CurrentPoints += int64(activity.TypicalDuration/2)
+      if activity.CurrentPoints.Valid {
+        activity.CurrentPoints.Int64 += int64(activity.TypicalDuration.Int64 / 2)
+      } else {
+        activity.CurrentPoints.Valid = true;
+        activity.CurrentPoints.Int64 = activity.TypicalDuration.Int64
+      }
+      if activity.CurrentPoints.Int64 > 0 {
+        sumCurrentPoint = sumCurrentPoint + activity.CurrentPoints.Int64;
+      }
       UpdateActivity(activity);
     }
   }
 
+  //Randomally choose an activity (activities with bigger CurrentPoints have a bigger chance to be picked
+  var indexPoint int64 = rand.Int63n(sumCurrentPoint);
+  chosenActivityIndex := -1;
+  for (indexPoint > 0) {
+    chosenActivityIndex += 1;
+    activity := schedulableActivities[chosenActivityIndex];
+    if activity.CurrentPoints.Int64 > 0 {
+      indexPoint -= activity.CurrentPoints.Int64;
+    }
+  }
+
+
   return domain.Journallog{1, time.Now(), "new",
-    domain.JsonNullInt64{sql.NullInt64{Int64:int64(schedulableActivities[0].Id), Valid:true}},
-    domain.JsonNullInt64{sql.NullInt64{Int64:1, Valid:false}},
-    schedulableActivities[0].Name, ""};
+    domain.JsonNullInt64{sql.NullInt64{Int64:int64(schedulableActivities[chosenActivityIndex].Id), Valid:true}},
+    domain.JsonNullInt64{sql.NullInt64{Int64:-1, Valid:false}},
+    schedulableActivities[chosenActivityIndex].Name, ""};
 }
